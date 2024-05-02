8cbc504f3a57beeb1fc00aaedc421ed73e87b7ff
Semyon <158074352+Psychopomp-Semargl@users.noreply.github.com>
2024-05-02 22:07:11 +0500

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,10 @@
+def say_hi():
+    print('Hello, world')
+print(say_hi())
+
+def say_hi2(value):
+    if value > 0:
+        print('Hello, world')
+    else:
+        print('Hello')
+print(say_hi2(-1))
